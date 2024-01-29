e827f50456fcd57590f9a64133ea219d3cfc697a
Denis Shuiskov <shuiskov2000@gmail.com>
2024-01-29 06:32:51 +0300

    all_done_tasks_lesson_2:task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task_01
+
+// Напишем программу, которая на вход
+// пронимает два целых числа и проверяет,
+// является ли первое число корнем второго.
+// a = 25, b = 5 => да
+// a = 2, b = 10 => нет
+// a = 9, b = -3 => да 
+// a = -3, b = 9 => нет 
+
+/*
+int firstNumber = 25; 
+int secondNumber = 5;
+
+if (secondNumber * secondNumber == firstNumber)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+//////////////////////////////////////////////////
+
+// task_02
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+/*
+int N = 4;
+int negativeN = -N;
+
+while (negativeN <= N)
+{
+    Console.Write(negativeN + " ");
+    negativeN++;
+}
+*/
+//////////////////////////////////////////////////
+
+// task_03
+// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+// int number = 435;
+// int firstDigit = number / 100;
+// int thirdDigit = number % 10;
+// int res = firstDigit + thirdDigit;
+// Console.WriteLine("Результат -> " + res);
+//////////////////////////////////////////////////
+
+// task_04v.01
+// На входе имеетcя три числа, найти максимальное из них.
+
+// int a = 7;
+// int b = 2;
+// int c = 3;
+// int max = 0;
+// char numberNameMax = 'a';
+
+// if (max < a){
+//     max = a;
+//     numberNameMax = 'a';
+// };
+
+// if (max < b){
+//     max = b;
+//     numberNameMax = 'b';
+// };
+
+// if (max < c){
+//     max = c;
+//     numberNameMax = 'c';
+// }
+
+// Console.WriteLine("Максимальное число -> "+ numberNameMax + " = " + max);
+
+// task_04_v.02
+
+// int a = 5;
+// int b = 6;
+// int c = 7; 
+
+// if (a > b && a > c){
+//     Console.WriteLine(a);
+// }
+// else if (b > a && b > c){
+//     Console.WriteLine(b);
+// }
+// else if (c > a && c > b){
+//     Console.WriteLine(c);
+// }
+
+////////////////////////////////////////////////////////////////////////////
+
+// task_05
+// На входе имеется число(number), а на выходе выводит все 
+// четные числа от 1 до number (включительно), после каждого 
+// числа должен быть знак пробела.
+
+// int number = 6;
+// int i = 1;
+
+// while (number > i){
+//     if (i % 2 == 0){
+//         Console.Write(i + " ");
+//     }
+//     i++;
+// }
--- task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
+++ task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad2e67f107be6b62bf80b796571a880b212ea8e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
